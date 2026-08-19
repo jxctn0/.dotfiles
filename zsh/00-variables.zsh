33d9621e3e0a406a75d2f713c9c5bbb5414fd755
jxctn0 <jxctno@gmail.com>
2026-08-19 18:32:19 +0100

    updated variables

--- zsh/00-variables.zsh
+++ zsh/00-variables.zsh
@@ -2,3 +2,4 @@
 PROMPT_PROVIDER="starship.zsh" # | starship.zsh | spaceship-pure.zsh etc for whatever prompt system you use, or leave blank for none
 NERDFONT_ENABLED=true # Set to false to disable nerd font icons
 EMOJI_FALLBACK=true # Set to false to disable emoji fallback (will use text instead of emojis if true and nerd fonts are not available)
+HOMEBREW_NO_UPDATE_REPORT_NEW=true
